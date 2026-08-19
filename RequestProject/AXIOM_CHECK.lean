c12f19b950a57396e1ae29c8ccff11b48b4a3a82
rain1 <rain1@airmail.cc>
2026-08-19 18:03:14 +0100

    Axiom check

---
+++ RequestProject/AXIOM_CHECK.lean
@@ -0,0 +1,5 @@
+import RequestProject.Assembly
+
+/-- info: 'Catalan.catalan_worthiness_uncond_catalan' depends on axioms: [propext, Classical.choice, Quot.sound] -/
+#guard_msgs in
+#print axioms Catalan.catalan_worthiness_uncond_catalan
